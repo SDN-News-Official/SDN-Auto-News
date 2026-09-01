a3b01638617d02eca503688798458d0d6872d888
Jamil Ahmed Kalyal <223995654+marwajameel@users.noreply.github.com>
2026-09-01 17:56:46 +0500

    Update deploy_with_web3.ts

--- deploy_with_web3.ts
+++ deploy_with_web3.ts
@@ -12,4 +12,29 @@
 		"stateMutability": "pure",
 		"type": "function"
 	}
-]
+]import { deploy } from './web3-lib'
+
+const contractABI = [
+	{
+		"inputs": [],
+		"name": "print",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "pure",
+		"type": "function"
+	}
+];
+
+(async () => {
+    try {
+        const result = await deploy('HelloWorld3', [], { abi: contractABI })
+        console.log(`address: ${result.address}`)
+    } catch (e) {
+        console.log(e.message)
+    }
+})()
